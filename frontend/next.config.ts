33f30de7494a9ade32da22b613e5e209f3c695ff
akash <wizardmakes@gmail.com>
2025-03-31 22:41:56 +0530

    vercel err

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' http: https: data: blob: 'unsafe-inline' 'unsafe-eval'",
+          },
+        ],
+      },
+    ];
+  },
+
   output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
